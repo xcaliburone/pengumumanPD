546207dc5c91b7e1171bc8a183c55310b2b51df1
adhimrahman <adhimrahaman@gmail.com>
2024-05-05 04:15:56 +0800

    .

--- server/index.js
+++ server/index.js
@@ -43,13 +43,13 @@ db.connect((err) => { if (err) throw err; console.log('Connected to MySQL databa
 // });
 
 // Middleware untuk menambahkan header CORS
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://pengumuman-peserta-didik.vercel.app');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// })
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://pengumuman-peserta-didik.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
 
 app.get('/', (req, res) => {
     return res.redirect('/App')
