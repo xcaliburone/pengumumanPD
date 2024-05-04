dc541edc59defad5097ffc2aec4928f185efba01
adhimrahman <adhimrahaman@gmail.com>
2024-05-05 04:21:17 +0800

    .

--- server/index.js
+++ server/index.js
@@ -1,67 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const mysql = require('mysql')
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 const path = require('path');
 
-// middleware
 const app = express();
 app.use(express.json());
-// app.use(cors(
-//     {
-//         origin: ["pengumuman-pd.vercel.app"],
-//         methods: ["POST", "GET"],
-//         credentials: true
-//     }
-// ));
-
 app.use(cors({
-    origin: ['https://pengumuman-peserta-didik.vercel.app'],
+    origin: 'https://pengumuman-peserta-didik.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true
 }));
-
-
 app.use(express.static(path.join(__dirname, '../client/public')));
 
 const urlDB = `mysql://root:qTcTfsbDSailPmAexJzbjpbVjFFqvMIv@monorail.proxy.rlwy.net:43679/railway`;
-
-// const urlDB = `mysql://root:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.MYSQL_DATABASE}`;
-
 const db = mysql.createConnection(urlDB);
-
 db.connect((err) => { if (err) throw err; console.log('Connected to MySQL database'); });
 
-// const db = mysql.createConnection({
-//     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     port: process.env.MYSQL_PORT,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE
-// }); db.connect((err) => { if (err) throw err; console.log('Connected to MySQL database'); });
-
-// Endpoint untuk API
-// app.get('/api/data', (req, res) => {
-//     const data = {
-//         message: 'Ini adalah data contoh dari server!'
-//     };
-//     res.json(data);
-// });
-
-// Middleware untuk menambahkan header CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://pengumuman-peserta-didik.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next(); 
-})
-
 app.get('/', (req, res) => {
-    return res.redirect('/App')
-    // return res.send("WAKAKAKKAKAK")
-})
+    return res.redirect('/App');
+});
 
 app.get('/siswa/:nomorUjian', (req, res) => {
     const nomorUjian = req.params.nomorUjian;
@@ -99,37 +57,5 @@ app.get('/cek-siswa/:nomorUjian', (req, res) => {
     });
 });
 
-
-// app.get('/siswa/:nomorUjian', (req, res) => {
-//     const nomorUjian = req.params.nomorUjian;
-//     const siswa = siswaData.find(siswa => siswa.noPeserta === nomorUjian);
-//     if (!siswa) {
-//         return res.send("Nomor ujian tidak ditemukan");
-//     }
-//     return res.json(siswa);
-// });
-
-// app.get('/cek-siswa/:nomorUjian', (req, res) => {
-//     const nomorUjian = req.params.nomorUjian;
-//     const siswa = siswaData.find(siswa => siswa.noPeserta === nomorUjian);
-//     if (!siswa) {
-//         return res.send("Nomor ujian tidak ditemukan");
-//     }
-    
-//     const { keterangan } = siswa;
-
-//     if (keterangan === 'LULUS') {
-//         return res.redirect('/PageLulus');
-//     } else if (keterangan === 'LULUS BERSYARAT') {
-//         return res.redirect('/PageLulusDengan');
-//     } else if (keterangan === 'KELULUSAN DITUNDA') {
-//         return res.redirect('/PageLulusTunda');
-//     } else {
-//         return res.send("Keterangan tidak valid");
-//     }
-// });
-
-
-
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}....`))
+app.listen(port, () => console.log(`Listening on port ${port}....`));
